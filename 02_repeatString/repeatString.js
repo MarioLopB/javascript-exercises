31262edfe56fd1b35e638a0e6eca41ae96831108
MarioLopB <mlopeb04@estudiantes.unileon.es>
2023-03-21 23:41:31 +0100

    Make exercise removeArray

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (buffer, n) {
+    let result = "";
 
+    if (n < 0) {
+        result = 'ERROR';
+    } else {
+        for (let i = 0; i < n; i++) {
+            result += buffer;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, n1 = "", n2 = "") {
+    let result = [];
 
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] != n1 || array[i] != n2){
+            result.push(array[i]);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
